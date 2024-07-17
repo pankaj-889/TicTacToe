fcc17dceeb980a6de122fad3f0383b9fda7b0c16
Pankaj Kumar <pankaj.kumar09_ftc@external.swiggy.in>
2024-07-17 14:14:02 +0530

    comments

--- main.go
+++ main.go
@@ -9,27 +9,26 @@ func main() {
 	game := Game{}
 	game.TicTacToeGame()
 	game.StartGame()
-
-	/*
-
-		Player
-		piece
-		turn
-
-
-		Board
-		size
-		grid of PlayingPiece
-
-		PieceType
-		enum
-
-		PlayingPiece
-		PieceType
-
-		Game
-		board
-		players
-		turn
-	*/
 }
+
+/*
+
+	Player
+	piece
+	turn
+
+	Board
+	size
+	grid of PlayingPiece
+
+	PieceType
+	enum
+
+	PlayingPiece
+	PieceType
+
+	Game
+	board
+	players
+	turn
+*/
